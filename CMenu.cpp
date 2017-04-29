6d5e55bebf3119310f44a774e7a26921e529914d
stepa0648 <stepikse@post.cz>
2017-04-30 00:45:52 +0200

    BezNcurse
    Pokus bezNcurses

--- CMenu.cpp
+++ CMenu.cpp
@@ -4,55 +4,51 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   CMenu.cpp
  * Author: severste
- * 
+ *
  * Created on April 12, 2017, 7:46 PM
  */
 
 #include "CMenu.h"
-#include <ncurses.h>
-
+#include <iostream>
+using namespace std;
 CMenu::CMenu() {
 }
 
 void CMenu::print() {
-    mvprintw(2, 20, "   _____                          _     _               _     ______    _ _ _                    __   ___  ");
-    mvprintw(3, 20, "  / ____|                        | |   | |             | |   |  ____|  | (_) |                  /_ | / _ \\ ");
-    mvprintw(4, 20, " | (___  _ __  _ __ ___  __ _  __| |___| |__   ___  ___| |_  | |__   __| |_| |_ ___  _ __  __   _| || | | |");
-    mvprintw(5, 20, "  \\___ \\| '_ \\| '__/ _ \\/ _` |/ _` / __| '_ \\ / _ \\/ _ \\ __| |  __| / _` | | __/ _ \\| '__| \\ \\ / / || | | |");
-    mvprintw(6, 20, "  ____) | |_) | | |  __/ (_| | (_| \\__ \\ | | |  __/  __/ |_  | |___| (_| | | || (_) | |     \\ V /| || |_| |");
-    mvprintw(7, 20, " |_____/| .__/|_|  \\___|\\__,_|\\__,_|___/_| |_|\\___|\\___|\\__| |______\\__,_|_|\\__\\___/|_|      \\_/ |_(_)___/ ");
-    mvprintw(8, 20, "        | |                                                                                                          ");
-    mvprintw(9, 20, "        |_|                                                                                                          ");
-
-    mvprintw(11, 2, "Menu: ");
-    mvprintw(12, 2, "1. Vytvorit novy spreadsheet");
-    mvprintw(13, 2, "2. Nacist spreadsheet ze souboru");
-    mvprintw(14, 2, "3. Konec");
-    refresh();
+    cout << "   _____                          _     _               _     ______    _ _ _                    __   ___  \n";
+    cout << "  / ____|                        | |   | |             | |   |  ____|  | (_) |                  /_ | / _ \\ \n";
+    cout << " | (___  _ __  _ __ ___  __ _  __| |___| |__   ___  ___| |_  | |__   __| |_| |_ ___  _ __  __   _| || | | |\n";
+    cout << "  \\___ \\| '_ \\| '__/ _ \\/ _` |/ _` / __| '_ \\ / _ \\/ _ \\ __| |  __| / _` | | __/ _ \\| '__| \\ \\ / / || | | |\n";
+    cout << "  ____) | |_) | | |  __/ (_| | (_| \\__ \\ | | |  __/  __/ |_  | |___| (_| | | || (_) | |     \\ V /| || |_| |\n";
+    cout << " |_____/| .__/|_|  \\___|\\__,_|\\__,_|___/_| |_|\\___|\\___|\\__| |______\\__,_|_|\\__\\___/|_|      \\_/ |_(_)___/ \n";
+    cout << "        | |                                                                                                          \n";
+    cout << "        |_|                                                                                                          \n";
+    cout << endl;
+    cout << "Menu: \n";
+    cout << "1. Vytvorit novy spreadsheet\n";
+    cout << "2. Nacist spreadsheet ze souboru\n";
+    cout << "3. Informace\n";
+    cout << "4. Konec\n";
 }
 
 void CMenu::run() {
 
-    mvprintw(15, 2, "");
     char c;
     while (1) {
-        c = getch(); // cekani na vstup od uzivatele a pote se vypne
-        mvprintw(15, 2, "");
-        if (c != '1' && c != '2' && c != '3') {
-            mvprintw(16, 2, "Stisknete klavesu 1, 2, nebo 3");
-            mvprintw(15, 2, "");
+        cin >> c;
+        if (c != '1' && c != '2' && c != '3' && c != '4') {
+            //vypisuje se tolikrat kolik je chybnych znaku opravit
+            cout << "Stisknete klavesu 1, 2, 3, nebo 4" << endl;
         }
 
-        if (c == '3') {
-            mvprintw(15, 2, "%c", c);
+        if (c == '4') {
             break;
         }
     }
-    refresh();
-    mvprintw(16, 2, "Stisknete jakoukoli klavesu k ukonceni");
+    cout << "Konec" << endl;
 
 }
 
@@ -61,4 +57,3 @@ CMenu::CMenu(const CMenu& orig) {
 
 CMenu::~CMenu() {
 }
-
