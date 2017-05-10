bdb0f3fb0d82b436421ed0d9811bcfcd5bc904ab
stepa0648 <stepikse@post.cz>
2017-05-10 16:09:00 +0200

    Funkční nekonečná tabulka

--- CTable.cpp
+++ CTable.cpp
@@ -15,62 +15,70 @@ using namespace std;
 
 //CRow==========================================================================
 
-CRow::CRow() : width(0) {
-    m_row.resize(10);
+CRow::CRow() : width(10) {
+    m_row.resize( width+1, CCell(""));
 }
 
 CCell & CRow::operator[](size_t pos){
-    if (pos > width) {
-        width = pos;
-    }
+
     if ( m_row.size() <= pos ) {
-        m_row.resize(2 * pos);
+        m_row.resize(2 * pos, CCell(""));
+        //cout << "m_row resized " << m_row.size() << " " << pos << endl;
     }
     return m_row[pos];
 }
 
-void CRow::print(size_t cnt) {
-    std::cout << std::setw(10) << cnt << "|";
-    for (size_t i = 0; i <= width; i++) {
-        std::cout << std::setw(10) << m_row[i] << "|";
+void CRow::print(size_t cnt, size_t x) {
+    if( x+11 >= m_row.size() ){
+      m_row.resize(2 * (x+11), CCell(""));
     }
-    std::cout << std::endl;
+    cout << setw(10) << cnt << "|"; //print number of the row in front of the row
+    for (size_t i = x; i <= x+10; i++) {
+        cout << setw(10) << m_row[i] << "|";
+    }
+    cout << endl;
 }
 
 //CTable========================================================================
 
-CTable::CTable() : width(0), height(0) {
-    m_table.resize(10);
+CTable::CTable() : width(10), height(10) {
+    m_table.resize( 11, CRow() );
 }
 
-void CTable::insert(size_t y, size_t x, CCell val) {
-    if (y + 1 > height) {
-        height = y + 1;
-    }
-    if (x + 1 > width) {
-        width = x + 1;
-    }
+void CTable::insert(size_t y, size_t x, const CCell & val) {
+    //if table height is lower than y resize the tabl height;
     if (m_table.size() <= y) {
-        m_table.resize(2 * y);
+        m_table.resize(2 * y, CRow() );
     }
+
     (m_table[y])[x] = val;
 }
 
-void CTable::print() {
-    cout << setw(10) << " " << "|";
-    for (size_t i = 0; i < width; i++) {
+void CTable::print(size_t y, size_t x) {
+
+  cout << "m_table size:  " << m_table.size() << " y+10= " << y+10 << endl;
+  if (m_table.size() <= y+10) {
+      m_table.resize(2 * (y+10), CRow() );
+      cout << "m_table resized " << m_table.size() << " " << y << endl;
+  }
+
+
+
+    cout << setw(10) << " " << "|"; //prints empty cell in left top corner
+    for (size_t i = x; i <= x+10; i++) {
         cout << setw(10) << i << "|";
     }
-    std::cout << std::endl;
-    for (size_t i = 0; i <= width; i++) {
+    cout << endl;
+    for (size_t i = 0; i <= 11; i++) {
         for (int j = 0; j < 10; j++) {
             cout << "_";
         }
         cout << "|";
     }
     cout << endl;
-    for (size_t i = 0; i < height; i++) {
-        m_table[i].print(i);
+
+    for (size_t i = y; i <= y+10; i++) {
+        m_table[i].print(i, x);
     }
-    cout << width << "x" << height << endl;
+    cout << "Width: " << width << " Height: " << height << endl;
 }
